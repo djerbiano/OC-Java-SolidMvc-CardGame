6708ef1d5b7c1e7284debcdadd5903308f481d92
Saber Ghoudi <saberghoudi2222@hotmail.fr>
2025-12-13 22:37:50 +0100

    Create GameEvaluator class - Single Responsibility

--- src/com/openClassrooms/cardgame/controller/GameController.java
+++ src/com/openClassrooms/cardgame/controller/GameController.java
@@ -3,6 +3,7 @@ package com.openClassrooms.cardgame.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.openClassrooms.cardgame.games.GameEvaluator;
 import com.openClassrooms.cardgame.model.Deck;
 import com.openClassrooms.cardgame.model.Player;
 import com.openClassrooms.cardgame.model.PlayingCard;
@@ -10,119 +11,93 @@ import com.openClassrooms.cardgame.view.View;
 
 public class GameController {
 
-	enum GameState {
-		AddingPlayers, CardsDealt, WinnerRevealed;
-	}
+    enum GameState {
+        AddingPlayers, CardsDealt, WinnerRevealed;
+    }
 
-	Deck deck;
-	List<Player> players;
-	Player winner;
-	View view;
+    Deck deck;
+    List<Player> players;
+    Player winner;
+    View view;
 
-	GameState gameState;
+    GameState gameState;
 
-	public GameController(Deck deck, View view) {
-		super();
-		this.deck = deck;
-		this.view = view;
-		this.players = new ArrayList<Player>();
-		this.gameState = GameState.AddingPlayers;
-		view.setController(this);
-	}
+    public GameController(Deck deck, View view) {
+        super();
+        this.deck = deck;
+        this.view = view;
+        this.players = new ArrayList<Player>();
+        this.gameState = GameState.AddingPlayers;
+        view.setController(this);
+    }
 
-	public void run() {
-		while (gameState == GameState.AddingPlayers) {
-			view.promptForPlayerName();
+    public void run() {
+        while (gameState == GameState.AddingPlayers) {
+            view.promptForPlayerName();
 
-		}
+        }
 
-		switch (gameState) {
-		case CardsDealt -> view.pomptForFlip();
-		case WinnerRevealed -> view.promptForNewGame();
+        switch (gameState) {
+            case CardsDealt -> view.pomptForFlip();
+            case WinnerRevealed -> view.promptForNewGame();
 
-		}
-	}
+        }
+    }
 
-	public void addPlayer(String playerName) {
-		if (gameState == GameState.AddingPlayers) {
-			players.add(new Player(playerName));
-			view.showPlayerName(players.size(), playerName);
+    public void addPlayer(String playerName) {
+        if (gameState == GameState.AddingPlayers) {
+            players.add(new Player(playerName));
+            view.showPlayerName(players.size(), playerName);
 
-		}
+        }
 
-	}
+    }
 
-	public void startGame() {
-		if (gameState != GameState.CardsDealt) {
-			deck.shuffle();
-			int playerIndex = 1;
-			for (Player player : players) {
-				player.addCardToHand(deck.removeTopCard());
-				view.showFaceDownCardForPlayer(playerIndex++, player.getName());
+    public void startGame() {
+        if (gameState != GameState.CardsDealt) {
+            deck.shuffle();
+            int playerIndex = 1;
+            for (Player player : players) {
+                player.addCardToHand(deck.removeTopCard());
+                view.showFaceDownCardForPlayer(playerIndex++, player.getName());
 
-			}
-			gameState = GameState.CardsDealt;
+            }
+            gameState = GameState.CardsDealt;
 
-		}
-		this.run();
+        }
+        this.run();
 
-	}
+    }
 
-	public void flipCards() {
-		int playerIndex = 1;
-		for (Player player : players) {
-			PlayingCard pc = player.getCard(0);
-			pc.flip();
-			view.showCardForPlayer(playerIndex++, player.getName(), pc.getRank().toString(), pc.getSuit().toString());
-		}
-		evaluateWinner();
-		displayWinner();
-		rebuildDeck();
-		gameState = GameState.WinnerRevealed;
-		this.run();
+    public void flipCards() {
+        int playerIndex = 1;
+        for (Player player : players) {
+            PlayingCard pc = player.getCard(0);
+            pc.flip();
+            view.showCardForPlayer(playerIndex++, player.getName(), pc.getRank().toString(), pc.getSuit().toString());
+        }
+        evaluateWinner();
+        displayWinner();
+        rebuildDeck();
+        gameState = GameState.WinnerRevealed;
+        this.run();
 
-	}
+    }
 
-	void evaluateWinner() {
+    void evaluateWinner() {
 
-		Player bestPlayer = null;
-		int bestRank = 1;
-		int bestSuit = -1;
+        winner = new GameEvaluator().evaluateWinner(players);
 
-		for (Player player : players) {
-			boolean newBestPlayer = false;
+    }
 
-			if (bestPlayer == null) {
-				newBestPlayer = true;
-			} else {
-				PlayingCard pc = player.getCard(0);
-				int thisRank = pc.getRank().value();
-				if (thisRank >= bestRank) {
-					newBestPlayer = true;
-				} else {
-					if (pc.getSuit().value() > bestSuit) {
-						newBestPlayer = true;
-					}
-				}
-			}
-			if (newBestPlayer) {
-				bestPlayer = player;
-				PlayingCard pc = player.getCard(0);
-				bestRank = pc.getRank().value();
-				bestSuit = pc.getSuit().value();
-			}
-		}
-		winner = bestPlayer;
-	}
+    void displayWinner() {
+        view.showWinner(winner.getName());
+    }
 
-	void displayWinner() {
-		view.showWinner(winner.getName());
-	}
-
-	void rebuildDeck() {
-		for (Player player : players) {
-			deck.returnCardToDeck(player.removeCard());
-		}
-	}
+    void rebuildDeck() {
+        for (Player player : players) {
+            deck.returnCardToDeck(player.removeCard());
+        }
+    }
 
 };
--- src/com/openClassrooms/cardgame/model/PlayingCard.java
+++ src/com/openClassrooms/cardgame/model/PlayingCard.java
@@ -1,32 +1,31 @@
 package com.openClassrooms.cardgame.model;
 
 public class PlayingCard {
-	private Rank rank;
-	private Suit suit;
+    private Rank rank;
+    private Suit suit;
+    private boolean faceUp;
 
-	private boolean faceUp;
+    public PlayingCard(Rank rank, Suit suit) {
+        super();
+        this.rank = rank;
+        this.suit = suit;
+    }
 
-	public PlayingCard(Rank rank, Suit suit) {
-		super();
-		this.rank = rank;
-		this.suit = suit;
-	}
+    public Rank getRank() {
+        return rank;
+    }
 
-	public Rank getRank() {
-		return rank;
-	}
+    public Suit getSuit() {
+        return suit;
+    }
 
-	public Suit getSuit() {
-		return suit;
-	}
+    public boolean isFaceUp() {
+        return faceUp;
+    }
 
-	public boolean isFaceUp() {
-		return faceUp;
-	}
-
-	public boolean flip() {
-		faceUp = !faceUp;
-		return faceUp;
-	}
+    public boolean flip() {
+        faceUp = !faceUp;
+        return faceUp;
+    }
 
 }
